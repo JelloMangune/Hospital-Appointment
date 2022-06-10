3964d12291c7fa0df080f752d9c00287effcc919
JelloMangune <enugnam.ollej@gmail.com>
2022-06-10 14:38:30 +0800

    bugs fixed

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -202,14 +202,13 @@
     <div class="container py-5 py-lg-0">
       <div class="row align-items-center">
         <div class="col-lg-4 wow zoomIn">
-          <div class="img-banner d-none d-lg-block">
-            <img src="../assets/img/mobile_app.png" alt="">
+          <div class="img-banner d-none d-lg-block" style="padding-right:700px;">
+            <img src="../assets/img/computer.png" style="width:500px;" alt="">
           </div>
         </div>
-        <div class="col-lg-8 wow fadeInRight">
-          <h1 class="font-weight-normal mb-3">Get easy access of all features using One Health Application</h1>
-          <a href="#"><img src="../assets/img/google_play.svg" alt=""></a>
-          <a href="#" class="ml-2"><img src="../assets/img/app_store.svg" alt=""></a>
+        <div class="col-lg-8 wow fadeInRight" style="padding-left:120px;">
+          <h1 class="font-weight-normal mb-3">Schedule an appointment using AppointMed Website</h1>
+          <h1 class="font-weight-normal mb-3" style="font-size:20px">visit us at www.apptmed.com</h1>
         </div>
       </div>
     </div>
